03140f3ac01d58c2850db832ff3c3ffd07fcc5f4
niuchong <niuchong@360.cn>
2022-11-01 17:10:43 +0800

    给Ground地面添加Tag, 添加导航移动的代码

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -11,4 +12,31 @@ public class EventVector3 : UnityEvent<Vector3>
 public class MouseManager : MonoBehaviour
 {
     public EventVector3 onMouseClicked;
+    RaycastHit hitInfo;
+
+    void Update()
+    {
+        SetCursorTexture();
+        MouseControl();
+    }
+
+    void SetCursorTexture()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hitInfo)) 
+        {
+             //«–ªª Û±ÍÃ˘Õº
+        }
+    }
+
+    void MouseControl()
+    {
+        if (Input.GetMouseButtonDown(0) && hitInfo.collider != null)
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Ground")) 
+            {
+                onMouseClicked?.Invoke(hitInfo.point);
+            }
+        }
+    }
 }
