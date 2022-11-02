565353816cdcc92a7c687bb83a1e4eaad8d97494
niuchong <niuchong@360.cn>
2022-11-02 11:14:57 +0800

    修改游戏控制器的脚本，根据人物的速度，切换不同的动画

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -6,15 +6,29 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator anim;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     void Start()
     {
         MouseManager.Instance.onMouseClicked += MoveToTarget;
+    }
+
+    void Update()
+    {
+        SwitchAnimation();
+    }
+
+    //切换人物跑动的动画
+    private void SwitchAnimation() 
+    {
+        //Vector3的数值转换成一个浮点的值通过sqrMagnitude
+        anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 
     public void MoveToTarget(Vector3 target)
