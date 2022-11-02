263c73db4e28ac0379512deea9a107004b208370
niuchong <niuchong@360.cn>
2022-11-02 15:40:57 +0800

    为敌人添加几种状态

--- Assets/Scripts/Characters/EnemyController.cs
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -1,15 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
+public enum EnemyStates { GUARD, PATROL, CHASE, DEAD }
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyController : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private EnemyStates enemyStates;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        SwitchStates();
+    }
+
+    void SwitchStates()
+    {
+        switch (enemyStates)
+        {
+            case EnemyStates.GUARD:
+                break;
+            case EnemyStates.PATROL:
+                break;
+            case EnemyStates.CHASE:
+                break;
+            case EnemyStates.DEAD:
+                break;
+        }
     }
 }
