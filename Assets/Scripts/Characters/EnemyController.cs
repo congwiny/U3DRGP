70cd6feaeef0e10a53b7ac0f9da0db87b05b9686
niuchong <niuchong@360.cn>
2022-11-02 15:27:12 +0800

    Slime关联EnemyController，设置Nav Mesh Agent

---
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+}
