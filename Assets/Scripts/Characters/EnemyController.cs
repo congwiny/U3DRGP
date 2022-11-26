936110d36eb72ae84e946fd25d586f6acaec9f6b
niuchong <niuchong@360.cn>
2022-11-26 17:57:28 +0800

    敌人追击Player的动画控制器

--- Assets/Scripts/Characters/EnemyController.cs
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -9,29 +9,50 @@ public class EnemyController : MonoBehaviour
     private NavMeshAgent agent;
     private EnemyStates enemyStates;
 
+    private Animator animator;
+
     //设置是巡逻的敌人还是站桩的敌人
     public bool isGuard;
 
     [Header("Basic Settings")]
     public float sightRadius;
 
+    //敌人最初始的移动速度，如果不是追击状态，则敌人的速度是此值的一半
+    private float originSpeed;
+
     private GameObject attackTarget;
+
+    //bool值配合动画的转换
+    private bool isWalk;
+    private bool isChase;
+    private bool isFollow;
+
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        originSpeed = agent.speed;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         SwitchStates();
+        SwitchAnimation();
+    }
+
+    void SwitchAnimation()
+    {
+        animator.SetBool("Walk", isWalk);
+        animator.SetBool("Chase", isChase);
+        animator.SetBool("Follow", isFollow);
     }
 
     void SwitchStates()
     {
         //如果发现Player，切换到追击的状态
         if (FoundPlayer())
-        { 
+        {
             enemyStates = EnemyStates.CHASE;
             Debug.Log("找到Player...");
         }
@@ -43,15 +64,20 @@ public class EnemyController : MonoBehaviour
                 break;
             case EnemyStates.CHASE:
                 //TODO:追Player
-              
+
                 //TODO:在攻击范围内则攻击
                 //TODO:配合动画
-                if(!FoundPlayer())
+                isWalk = false;
+                isChase = true;
+                agent.speed = originSpeed;
+                if (!FoundPlayer())
                 {
                     //TODO:拉脱回到上一个状态
+                    isFollow = false;
                 }
                 else
                 {
+                    isFollow = true;
                     agent.destination = attackTarget.transform.position;
                 }
                 break;
@@ -63,7 +89,7 @@ public class EnemyController : MonoBehaviour
 
     //查找周围的Player
     bool FoundPlayer()
-    { 
+    {
         var colliders = Physics.OverlapSphere(transform.position, sightRadius);
         foreach (var target in colliders)
         {
